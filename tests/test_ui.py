34dfc0e74a2fe975c682cc9178c4e2a697ddca0c
Alberto Rivas <alberto.rivas@seeq.com>
2021-12-08 17:49:57 -0600

    updated test_config.ini

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -31,6 +31,7 @@ def system_test_setup():
 
 @pytest.mark.system
 def test_ui_instance(system_test_config, system_test_setup):
+    # This test doesn't test the rendering of the UI, just the instantiation of the UI
     notebook = f'{spy.utils.get_data_lab_project_url()}/dummy.ipynb?workbookId=' \
                f'{spy.utils.get_workbook_id_from_url(test_worksheet_url)}'
     C = azureml.MlOperate(config_file=test_common.TEST_CONFIG_FILE, sdl_notebook_url=notebook)  # sdl_notebook_url
